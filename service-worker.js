b5051df62412c3291641735430b8af7b0efca25d
Volodymyr Khimiak <vv.himiak@gmail.com>
2021-08-31 20:56:06 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.4dddc0e76f714a8bf2988271788f290e.js"
 );
 
 self.addEventListener('message', (event) => {
